1e491dc2c38b1b8b21ffb49dfd3de2963e21af6d
neglux <akcaaytuna@gmail.com>
2022-07-09 11:36:11 +0300

    develop: adding home page strings

---
+++ src/data/strings.js
@@ -0,0 +1,20 @@
+export default {
+  searchBox: {
+    placeholder: "Search by country name",
+    maxLength: "50",
+  },
+  country: {
+    map: {
+      title: "map",
+    },
+    neighbors: {
+      title: "neighbors",
+    },
+    info: {
+      title: "country info",
+    },
+    capital: {
+      title: "capital",
+    },
+  },
+};
