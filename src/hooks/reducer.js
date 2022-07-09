81f5a4b6ec3d2bc3a6698a2b597c698fd286f2fb
neglux <akcaaytuna@gmail.com>
2022-07-09 06:57:53 +0300

    develop: structure #2

---
+++ src/hooks/reducer.js
@@ -0,0 +1,8 @@
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "":
+      return { ...state };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
