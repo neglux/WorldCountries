06d1cce0509bdb8ba7f7c7c3007bc6dbb9cfbe4a
neglux <akcaaytuna@gmail.com>
2022-07-09 11:35:11 +0300

    develop: designing

--- src/components/CountryLineBox.js
+++ src/components/CountryLineBox.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const CountryLineBox = () => {
-  return <div>CountryLineBox</div>;
+  return (
+    <div className="flex justify-start items-center w-[300px] h-[40px] bg-darkgray-500 rounded-md shadow-md text-sm cursor-pointer hover:bg-darkgray-300">
+      <img
+        className="w-[50px] h-[25px] ml-5 overflow-hidden object-contain"
+        // src="https://flagcdn.com/{country_code}.svg"
+        alt="country flag"
+      />
+      <h5 className="ml-5">Country Name</h5>
+    </div>
+  );
 };
 
 export default CountryLineBox;
