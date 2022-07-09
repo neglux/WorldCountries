f81ac1843fb8531094acdde7d7e914374b684080
neglux <akcaaytuna@gmail.com>
2022-07-09 11:35:37 +0300

    develop: designing home page

---
+++ src/components/Line.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Line = () => {
+  return <div className="w-full h-[1px] bg-darkgray-500 rounded-lg"></div>;
+};
+
+export default Line;
---
+++ src/pages/Home.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Page from "../components/Page";
+import SearchBox from "../components/SearchBox";
+import logo from "../assets/logo.svg";
+import CountryLineBox from "../components/CountryLineBox";
+import Line from "../components/Line";
+
+const Home = () => {
+  return (
+    <Page>
+      <div className="flex flex-col justify-start items-center h-full">
+        <div className="w-full mb-10">
+          <img
+            className="mt-40 mb-10 mx-auto"
+            src={logo}
+            alt="world countries logo"
+          />
+          <SearchBox />
+        </div>
+        <Line />
+        <ul className="grid grid-cols-4 gap-10 my-10 overflow-y-auto">
+          <li>
+            <CountryLineBox />
+          </li>
+          <li>
+            <CountryLineBox />
+          </li>
+          <li>
+            <CountryLineBox />
+          </li>
+          <li>
+            <CountryLineBox />
+          </li>
+          <li>
+            <CountryLineBox />
+          </li>
+        </ul>
+      </div>
+    </Page>
+  );
+};
+
+export default Home;
