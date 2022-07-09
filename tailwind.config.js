3f9001c62bd83f26e4325a77d854b8e2dbc99ed1
neglux <akcaaytuna@gmail.com>
2022-07-09 11:34:48 +0300

    develop: adding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      darkgray: {
+        100: "#D8D8D8",
+        300: "#707070",
+        500: "#4D4D4D",
+        700: "#303030",
+        900: "#1E1E1E",
+      },
+    },
   },
   plugins: [],
 };
