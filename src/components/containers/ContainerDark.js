93a9b312b17d45742d935cf54344741adcc0e39a
neglux <akcaaytuna@gmail.com>
2022-07-09 21:44:26 +0300

    develop: country page

--- src/components/containers/ContainerDark.js
+++ src/components/containers/ContainerDark.js
@@ -6,8 +6,8 @@ const ContainerDark = ({ children, title, style }) => {
     <div
       className={`flex flex-col bg-darkgray-700 ${style} mt-5 px-5 py-5 rounded-md shadow-md`}
     >
-      <div className="mb-10 w-full">
-        <h3 className="capitalize w-full mb-5">{title}</h3>
+      <div className="mb-5 w-full">
+        <h3 className="capitalize w-full mb-5 text-xl">{title}</h3>
         <Line />
       </div>
       {children}
---
+++ src/pages/Country.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ContainerDark from "../components/containers/ContainerDark";
+import ContainerLight from "../components/containers/ContainerLight";
+import Label from "../components/containers/Label";
+import CountryBox from "../components/CountryBox";
+import CountryLineBox from "../components/CountryLineBox";
+import Line from "../components/Line";
+import Page from "../components/Page";
+import SearchBox from "../components/SearchBox";
+import strings from "../data/strings";
+
+const Country = () => {
+  const { map, neighbors, info, capital } = strings.country;
+  return (
+    <Page>
+      <div>
+        <SearchBox />
+        <Line />
+        <CountryBox />
+        <div className="flex justify-between items-baseline">
+          <ContainerDark title={map.title} style="w-3/5"></ContainerDark>
+          <ContainerDark title={neighbors.title} style="w-1/3 h-fit">
+            <ul>
+              <li>
+                <CountryLineBox />
+              </li>
+            </ul>
+          </ContainerDark>
+        </div>
+        <div className="flex justify-between items-baseline">
+          <ContainerDark title={info.title} style="w-3/5">
+            <div className="mb-5">
+              <Label Label={info.meta.population} />
+              <Label Label={info.meta.area} />
+            </div>
+            <Line />
+            <div className="my-5">
+              <Label Label={info.economy.currency} />
+              <Label Label={info.economy.gini} />
+            </div>
+            <Line />
+            <div>
+              <p></p>
+            </div>
+          </ContainerDark>
+          <ContainerDark title={capital.title} style="w-1/3">
+            <ContainerLight></ContainerLight>
+          </ContainerDark>
+        </div>
+      </div>
+      <footer className="h-[100px]"></footer>
+    </Page>
+  );
+};
+
+export default Country;
