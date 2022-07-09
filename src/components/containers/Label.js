3e129fa37004d07a4a67d139dd8a33a7abd6db89
neglux <akcaaytuna@gmail.com>
2022-07-09 21:42:26 +0300

    develop: label

---
+++ src/components/containers/Label.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Label = ({ Label }) => {
+  return (
+    <h4 className="text-lg capitalize my-1">
+      {Label}:<span></span>
+    </h4>
+  );
+};
+
+export default Label;
