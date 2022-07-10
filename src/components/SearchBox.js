a4d310500f2cdeffdc2f96f2f73a3c4737b6b462
neglux <akcaaytuna@gmail.com>
2022-07-10 13:08:00 +0300

    develop: searching functionality

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from "react";
 import strings from "../data/strings";
 import { AiOutlineSearch } from "react-icons/ai";
 
-const SearchBox = () => {
+const SearchBox = ({ value, changeHandler, clickHandler }) => {
   const { placeholder, maxLength } = strings.searchBox;
   return (
     <div className="flex justify-between items-center w-2/4 h-[50px] mb-5 bg-darkgray-500 rounded-lg shadow-lg mx-auto">
@@ -11,8 +11,12 @@ const SearchBox = () => {
         type="text"
         placeholder={placeholder}
         maxLength={maxLength}
+        value={value}
+        onChange={(e) => {
+          changeHandler(e.target.value);
+        }}
       />
-      <button className="text-2xl px-5">
+      <button className="text-2xl px-5" onClick={clickHandler}>
         <AiOutlineSearch />
       </button>
     </div>
