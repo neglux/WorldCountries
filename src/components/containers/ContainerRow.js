23ba241e153def85ab1dc8d1deb51ad72c735a60
neglux <akcaaytuna@gmail.com>
2022-07-10 08:35:52 +0300

    fix: adding row container

---
+++ src/components/containers/ContainerRow.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContainerRow = ({ children }) => {
+  return <div className="flex justify-between mt-8">{children}</div>;
+};
+
+export default ContainerRow;
--- src/pages/Country.js
+++ src/pages/Country.js
@@ -8,46 +8,45 @@ import Line from "../components/Line";
 import Page from "../components/containers/Page";
 import SearchBox from "../components/SearchBox";
 import strings from "../data/strings";
+import ContainerRow from "../components/containers/ContainerRow";
 
 const Country = () => {
   const { map, neighbors, info, capital } = strings.country;
   return (
     <Page>
-      <div>
-        <SearchBox />
-        <Line />
-        <CountryBox />
-        <div className="flex justify-between items-baseline">
-          <ContainerDark title={map.title} style="w-3/5"></ContainerDark>
-          <ContainerDark title={neighbors.title} style="w-1/3 h-fit">
-            <ul>
-              <li>
-                <CountryLineBox />
-              </li>
-            </ul>
-          </ContainerDark>
-        </div>
-        <div className="flex justify-between items-baseline">
-          <ContainerDark title={info.title} style="w-3/5">
-            <div className="mb-5">
-              <Label Label={info.meta.population} />
-              <Label Label={info.meta.area} />
-            </div>
-            <Line />
-            <div className="my-5">
-              <Label Label={info.economy.currency} />
-              <Label Label={info.economy.gini} />
-            </div>
-            <Line />
-            <div>
-              <p></p>
-            </div>
-          </ContainerDark>
-          <ContainerDark title={capital.title} style="w-1/3">
-            <ContainerLight></ContainerLight>
-          </ContainerDark>
-        </div>
-      </div>
+      <SearchBox />
+      <Line />
+      <CountryBox />
+      <ContainerRow>
+        <ContainerDark title={map.title} style="w-3/5"></ContainerDark>
+        <ContainerDark title={neighbors.title} style="w-1/3">
+          <ul>
+            <li className="my-5">
+              <CountryLineBox />
+            </li>
+          </ul>
+        </ContainerDark>
+      </ContainerRow>
+      <ContainerRow>
+        <ContainerDark title={info.title} style="w-3/5">
+          <div className="mb-5">
+            <Label Label={info.meta.population} />
+            <Label Label={info.meta.area} />
+          </div>
+          <Line />
+          <div className="my-5">
+            <Label Label={info.economy.currency} />
+            <Label Label={info.economy.gini} />
+          </div>
+          <Line />
+          <div>
+            <p></p>
+          </div>
+        </ContainerDark>
+        <ContainerDark title={capital.title} style="w-1/3">
+          <ContainerLight></ContainerLight>
+        </ContainerDark>
+      </ContainerRow>
     </Page>
   );
 };
