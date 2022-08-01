c35978e3c3a2a01fca9973c67b8b1b69483ac661
neglux <akcaaytuna@gmail.com>
2022-08-01 11:29:25 +0300

    develop: handling errors

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,21 +2,27 @@ import React, { useState } from "react";
 import strings from "../data/strings";
 import CountryLineBox from "./containers/CountryLineBox";
 import { AiOutlineSearch } from "react-icons/ai";
+import { FaSpinner } from "react-icons/fa";
 import { useGlobalContext } from "../contexts/AppContext";
 
 const SearchBox = ({ isNavbarSearch }) => {
   const { placeholder, maxLength } = strings.searchBox;
   const { searchResults, setSearchResults } = useGlobalContext();
-  const [query, setQuery] = useState();
+  const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [isResultWindowOpen, setIsResultWindowOpen] = useState(false);
-  const url = `https://restcountries.com/v3.1/name/${query}`;
 
   async function fetchData() {
+    const url = `https://restcountries.com/v3.1/name/${query}`;
     try {
+      setIsLoading(true);
       const response = await fetch(url);
+      if (!response.ok) throw new Error("Not found");
       const fetchedData = await response.json();
       setSearchResults(fetchedData);
+      setIsLoading(false);
     } catch (error) {
+      setIsLoading(false);
       throw new Error(error);
     }
   }
@@ -29,7 +35,12 @@ const SearchBox = ({ isNavbarSearch }) => {
   }
   return (
     <div className="relative">
-      <div className="flex justify-between items-center w-2/4 h-[50px] mb-5 bg-darkgray-500 rounded-lg shadow-lg mx-auto">
+      <form
+        className="flex justify-between items-center h-[50px] mb-5 bg-darkgray-500 rounded-lg shadow-lg mx-auto sm:w-full lg:w-2/4"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <input
           className="px-5 flex-grow h bg-darkgray-500 outline-none border-r-2 border-darkgray-300"
           type="text"
@@ -52,9 +63,14 @@ const SearchBox = ({ isNavbarSearch }) => {
         >
           <AiOutlineSearch />
         </button>
-      </div>
+      </form>
+      {isLoading && !isNavbarSearch && (
+        <div className="w-full flex justify-center text-xl">
+          <FaSpinner className="animate-spin" />
+        </div>
+      )}
       {isNavbarSearch && isResultWindowOpen && (
-        <ul className="absolute grid grid-cols-3 gap-3 overflow-y-auto my-auto w-full h-[200px] bg-darkgray-700 rounded-lg shadow-lg px-10 py-5">
+        <ul className="absolute grid sm:grid-cols-1 lg:grid-cols-3 gap-3 overflow-y-auto my-auto w-full h-[200px] bg-darkgray-700 rounded-lg shadow-lg px-10 py-5">
           {searchResults?.status !== 404 &&
             searchResults.map((item, index) => {
               return (
