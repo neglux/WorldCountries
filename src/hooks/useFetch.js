681b14a4fb95dd763c75e9cb14d159a7ef2aea26
neglux <akcaaytuna@gmail.com>
2022-08-02 10:23:15 +0300

    develop: error handling

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -7,10 +7,12 @@ const useFetch = (url) => {
   async function fetchData() {
     try {
       const response = await fetch(url);
+      if (!response.ok) throw new Error("Not found");
       const fetchedData = await response.json();
       setData(fetchedData);
       setIsLoading(false);
     } catch (error) {
+      setIsLoading(false);
       throw new Error(error);
     }
   }
